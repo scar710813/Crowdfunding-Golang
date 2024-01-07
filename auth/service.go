4e0974b7a0a422b29a3bc06ab531fae45856d4d7
scar710813 <unclescar0813@gmail.com>
2024-01-07 23:29:09 +0700

    JWT auth, validate token

--- auth/service.go
+++ auth/service.go
@@ -1,10 +1,15 @@
 package auth
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"errors"
 
-// Generate JWT token
+	"github.com/dgrijalva/jwt-go"
+)
+
+// Generate and validate JWT token
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 type jwtService struct {
@@ -30,4 +35,22 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 	}
 
 	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Invalid token")
+		}
+
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
 }
