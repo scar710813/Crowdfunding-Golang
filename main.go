5eda7f2707fdc5bcc8164b15442addbd2e222d90
scar710813 <unclescar0813@gmail.com>
2024-01-14 23:28:53 +0700

    Midtrans notification

--- main.go
+++ main.go
@@ -36,13 +36,13 @@ func main() {
 	userService := user.NewService(userRepository)
 	campaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewService()
-	paymentService := payment.NewService()
+	paymentService := payment.NewService(transactionRepository, campaignRepository)
 	transactionService := transaction.NewService(transactionRepository, campaignRepository, paymentService)
 
 	// Handler
 	userHandler := handler.NewUserHandler(userService, authService)
 	campaignHandler := handler.NewCampaignHandler(campaignService)
-	transactionHandler := handler.NewTransactionHandler(transactionService)
+	transactionHandler := handler.NewTransactionHandler(transactionService, paymentService)
 
 	router := gin.Default()
 	router.Static("/images", "./images/")
@@ -62,6 +62,7 @@ func main() {
 	api.GET("/campaigns/:id/transactions", authMiddleware(authService, userService), transactionHandler.GetCampaignTransactions)
 	api.GET("/transactions", authMiddleware(authService, userService), transactionHandler.GetUserTransactions)
 	api.POST("/transactions", authMiddleware(authService, userService), transactionHandler.CreateTransaction)
+	api.POST("/transactions/notification", transactionHandler.GetNotification)
 
 	router.Run()
 }
--- transaction/input.go
+++ transaction/input.go
@@ -8,8 +8,15 @@ type GetCampaignTransactionsInput struct {
 }
 
 type CreateTransactionInput struct {
-	Amount     int `json:"amount" binding:"required"`
-	CampaignID int `json:"campaign_id" binding:"required"`
+	Amount     int    `json:"amount" binding:"required"`
+	CampaignID int    `json:"campaign_id" binding:"required"`
 	Code       string `json:"code" binding:"required"`
 	User       user.User
+}
+
+type TransactionNotificationInput struct {
+	TransactionStatus string `json:"transaction_status"`
+	OrderID           string `json:"order_id"`
+	PaymentType       string `json:"payment_type"`
+	FraudStatus       string `json:"fraud_status"`
 }
